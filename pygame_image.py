88d8e784909861b9be5108c878d35d4057078612
fujimoto yuuki <c0a24221ec@edu.teu.ac.jp>
2025-11-18 17:10:48 +0900

    練習9

--- pygame_image.py
+++ pygame_image.py
@@ -17,12 +17,13 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = tmr
+        x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2,[-x+1600,0])
+        screen.blit(bg_img, [-x+3200, 0])
         screen.blit(kk_img, [300,200] )
         pg.display.update()
-        tmr += 1        
+        tmr += 1       
         clock.tick(200)
 
 
